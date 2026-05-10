62bb3cae62a3c8749940ced8d8c93f3945d1628f
ADMIN <seklish9@gmail.com>
2026-05-10 17:58:16 +0200

    Demo version of the program is done, no test were done

--- Main_tea.py
+++ Main_tea.py
@@ -13,39 +13,116 @@ class Tea:
 
 class BlackTea(Tea):
     def __init__(self):
-        super.__init__("Black Tea", 95, 4)
+        super().__init__("Black Tea", 95, 4)
 
 class GreenTea(Tea):
     def __init__(self):
-        super.__init__("Green Tea", 80, 3)
+        super().__init__("Green Tea", 80, 3)
 
 class RedTea(Tea):
     def __init__(self):
-        super.__init__("Red Tea", 90, 5)
+        super().__init__("Red Tea", 90, 5)
 
 
 class BrewTea:
-    def __init__(self, tea, water_temp, water_time, sugar):
+    def __init__(self, tea, water_temp, water_time, sugar, lemon, milk_ratio):
         self.tea = tea
         self.water_temp = water_temp
         self.water_time = water_time
         self.sugar = sugar
+        self.lemon = lemon
+        self.milk_ratio = milk_ratio
+
+    def describe(self):
+        print(self.water_temp)
+        print(self.water_time)
+        print(self.sugar)
+        print(self.lemon)
+        print(self.milk_ratio)
 
 #giving the score to the tea
 
-class Evaluator:
-    def __init__(self, tea, water_temp, water_time, sugar):
-        pass
+class Conclusion:
+    def score(self, brewedtea):
+        score = 100
+
+        score -= abs(brewedtea.tea.op_temp - brewedtea.water_temp) / brewedtea.tea.op_temp * 100
+        score -= abs(brewedtea.tea.op_time - brewedtea.water_time) / brewedtea.tea.op_time * 100
+
+        print("Score of your tea: ", score)
+
+    class Conclusion:
+        @staticmethod
+        def score(brewedtea):
+            score = 100
+
+            score -= abs(brewedtea.tea.op_temp - brewedtea.water_temp) / brewedtea.tea.op_temp * 100
+            score -= abs(brewedtea.tea.op_time - brewedtea.water_time) / brewedtea.tea.op_time * 100
+
+            print("Score of your tea: ", score)
+
+        @staticmethod
+        def description(brewedtea):
+            print("Your tea will be:")
+
+            # Sugar analysis
+            if brewedtea.sugar == 0:
+                print("- unsweetened")
+            elif brewedtea.sugar == 1:
+                print("- a bit sweet")
+            elif brewedtea.sugar == 2:
+                print("- sweet")
+            elif brewedtea.sugar >= 3:
+                print("- very sweet")
+
+            print("Also, the taste is going to be:")
+
+            # Temperature analysis
+            temp_diff = brewedtea.water_temp - brewedtea.tea.op_temp
+            if temp_diff <= -10:
+                print("- weak and under-extracted due to insufficient water temperature.")
+            elif temp_diff >= 10:
+                print("- bitter and potentially burnt due to excessive water temperature.")
+            else:
+                print("- accurately extracted based on temperature.")
+
+            # Time analysis
+            time_diff = brewedtea.water_time - brewedtea.tea.op_time
+            if time_diff <= -1:
+                print("- watery due to an insufficient brewing duration.")
+            elif time_diff >= 2:
+                print("- astringent and over-steeped due to an excessive brewing duration.")
+            else:
+                print("- well-balanced based on the brewing time.")
+
+            # Lemon and Milk analysis
+            if brewedtea.lemon and brewedtea.milk_ratio > 0:
+                print(
+                    "- unpalatable and curdled. The acidity of the lemon has reacted adversely with the milk proteins.")
+            else:
+                if brewedtea.lemon:
+                    print("- citrusy and acidic due to the addition of lemon.")
+
+                if brewedtea.milk_ratio > 0:
+                    if brewedtea.milk_ratio <= 0.2:
+                        print("- creamy with a standard dash of milk.")
+                    elif brewedtea.milk_ratio <= 0.5:
+                        print("- quite milky, which will significantly dilute the primary tea flavour.")
+                    else:
+                        print("- predominantly milk, entirely overwhelming the tea infusion.")
+
 
 #tea factory
 
 class Factory:
     def create_tea(tea_type):
-        if tea_type == "BlackTea":
+        tea_type = tea_type.lower()
+
+        if tea_type == "black":
             return BlackTea()
-        elif tea_type == "GreenTea":
+        elif tea_type == "green":
             return GreenTea()
-        elif tea_type == "RedTea":
+        elif tea_type == "red":
             return RedTea()
         else:
             raise Exception("Unknown tea type")
---
+++ UI_tea.py
@@ -0,0 +1,87 @@
+from Main_tea import Conclusion, BrewTea, Factory #importing classes from main
+
+#asking user to create his own tea
+
+allowed_tea_types = ["black", "green", "red"]
+
+#tea type
+while True:
+    tea_type = input("Input tea type (or 'exit' to exit): ").lower()
+    if tea_type == "exit":
+        exit()
+
+    if tea_type in allowed_tea_types:
+        break
+    else:
+        print("wrong input")
+
+#temperature
+while True:
+    temp = input("Input temperature of water (or 'exit' to exit): ")
+    if temp.lower() == "exit":
+        exit()
+    if temp.isdigit() or (temp.startswith("-") and temp[1:].isdigit()):
+        temp = int(temp)
+        if 0 <= temp <= 100:
+            break
+    print("wrong input")
+
+#brewing time
+while True:
+    time = input("Input duration of brewing (or 'exit' to exit): ")
+    if time.lower() == "exit":
+        exit()
+    if time.isdigit() and int(time) >= 0:
+        time = int(time)
+        break
+    print("wrong input")
+
+#sugar
+while True:
+    sugar = input("Input amount of sugar (or 'exit' to exit): ")
+    if sugar.lower() == "exit":
+        exit()
+    if sugar.isdigit() and int(sugar) >= 0:
+        sugar = int(sugar)
+        break
+    print("wrong input")
+
+#lemon
+while True:
+    islemon = input("Do you want lemon (yes/no or 'exit' to exit): ").lower()
+    if islemon == "exit":
+        exit()
+    if islemon == "yes":
+        lemon = True
+        break
+    elif islemon == "no":
+        lemon = False
+        break
+    print("wrong input")
+
+#milk ratio
+while True:
+    milk_ratio = input("Input milk ratio (0-1 or 'exit'  to exit): ")
+    if milk_ratio.lower() == "exit":
+        exit()
+    try:
+        milk_ratio = float(milk_ratio)
+        if 0 <= milk_ratio <= 1:
+            break
+    except ValueError:
+        pass
+    print("wrong input")
+
+#creating object tea and outputing data about it
+
+tea = Factory.create_tea(tea_type)
+tea.describe()
+
+#brewing the tea and checking what is inside
+
+brewedtea = BrewTea(tea, temp, time, sugar, lemon, milk_ratio)
+print("Your tea:")
+brewedtea.describe()
+
+Conclusion.score(brewedtea)
+Conclusion.description(brewedtea)
