c1c444b132c6069cae463cc0dfc90ac35ca536a6
ADMIN <seklish9@gmail.com>
2026-05-27 14:19:23 +0200

    main logic was created

--- Main_tea.py
+++ Main_tea.py
@@ -1,108 +1,83 @@
 from abc import ABC, abstractmethod
 
-#creating classes
+#asking about user's preferences
 
-class Tea:
-    @abstractmethod
-    def __init__(self, name, op_temp, op_time):
+class Preferences:
+    def __init__(self, sweetness, sourness, bitterness, strength):
+        self.sweetness = sweetness
+        self.sourness = sourness
+        self.bitterness = bitterness
+        self.strength = strength
+
+
+#main abstract class tea
+
+class Tea(ABC):
+    def __init__(self, name, brew_temp, brew_time):
         self.name = name
-        self.op_temp = op_temp
-        self.op_time = op_time
+        self.brew_temp = brew_temp
+        self.brew_time = brew_time
 
-    def describe(self):
-        print(self.name)
-        print(self.op_temp)
-        print(self.op_time)
+    @abstractmethod
+    def match(self, prefs: Preferences) -> float:
+        pass
+#method, same as describe, used to know how to brew tea correctly
+    def brew_info(self):
+        return f"{self.brew_temp}°C, {self.brew_time} min"
+
+
+#classes with types of tea
 
 class BlackTea(Tea):
     def __init__(self):
         super().__init__("Black Tea", 95, 4)
+#matching method, that allows user to know score of his tea
+    def match(self, prefs):
+        score = 0
+        score += 10 - abs(prefs.strength - 9)
+        score += 10 - abs(prefs.bitterness - 7)
+        score += 10 - abs(prefs.sweetness - 3)
+        return score
 
+#pretty much the same as black tea, but logic differs
 class GreenTea(Tea):
     def __init__(self):
         super().__init__("Green Tea", 80, 3)
 
+    def match(self, prefs):
+        score = 0
+        score += 10 - abs(prefs.bitterness - 5)
+        score += 10 - abs(prefs.sourness - 4)
+        score += 10 - abs(prefs.strength - 4)
+        return score
+
+#pretty much the same as other teas, but logic differs
 class RedTea(Tea):
     def __init__(self):
         super().__init__("Red Tea", 90, 5)
 
+    def match(self, prefs):
+        score = 0
+        score += 10 - abs(prefs.strength - 6)
+        score += 10 - abs(prefs.sweetness - 5)
+        score += 10 - abs(prefs.sourness - 2)
+        return score
 
-class BrewTea:
-    def __init__(self, tea, water_temp, water_time, sugar, lemon):
-        self.tea = tea
-        self.water_temp = water_temp
-        self.water_time = water_time
-        self.sugar = sugar
-        self.lemon = lemon
 
-    def describe(self):
-        print(self.water_temp)
-        print(self.water_time)
-        print(self.sugar)
-        print(self.lemon)
+#class, that is choosing tea to recommend
 
-#giving the score to the tea
+class TeaRecommender:
+    def __init__(self):
+        self.teas = [BlackTea(), GreenTea(), RedTea()]
+#method, that is using preferences, entered before to choose tea with best score be matching them with the others
+    def recommend(self, prefs: Preferences):
+        best_tea = None
+        best_score = -1
 
-class Conclusion:
-    @staticmethod
-    def score(brewedtea):
-        score = 100
-
-        score -= abs(brewedtea.tea.op_temp - brewedtea.water_temp) / brewedtea.tea.op_temp * 100
-        score -= abs(brewedtea.tea.op_time - brewedtea.water_time) / brewedtea.tea.op_time * 100
-
-        print("Score of your tea: ", score)
-
-    @staticmethod
-    def description(brewedtea):
-        print("Your tea will be:")
-
-        #sugar analysis
-        if brewedtea.sugar == 0:
-            print("- unsweetened")
-        elif brewedtea.sugar == 1:
-            print("- a bit sweet")
-        elif brewedtea.sugar == 2:
-            print("- sweet")
-        elif brewedtea.sugar >= 3:
-            print("- very sweet")
-
-        print("Also, the taste is going to be:")
-
-        #temperature analysis
-        temp_diff = brewedtea.water_temp - brewedtea.tea.op_temp
-        if temp_diff <= -10:
-            print("- weak and under-extracted due to insufficient water temperature.")
-        elif temp_diff >= 10:
-            print("- bitter and potentially burnt due to excessive water temperature.")
-        else:
-            print("- accurately extracted based on temperature.")
-
-        #time analysis
-        time_diff = brewedtea.water_time - brewedtea.tea.op_time
-        if time_diff <= -1:
-            print("- watery due to an insufficient brewing duration.")
-        elif time_diff >= 2:
-            print("- astringent and over-steeped due to an excessive brewing duration.")
-        else:
-            print("- well-balanced based on the brewing time.")
-
-        #lemon analysis
-        if brewedtea.lemon == True:
-            print("- your tea is going to be a bit sour, perfect addition")
-
-#tea factory
-
-class Factory:
-    @staticmethod
-    def create_tea(tea_type):
-        tea_type = tea_type.lower()
-
-        if tea_type == "black":
-            return BlackTea()
-        elif tea_type == "green":
-            return GreenTea()
-        elif tea_type == "red":
-            return RedTea()
-        else:
-            raise Exception("Unknown tea type")
+        for tea in self.teas:
+            score = tea.match(prefs)  # POLYMORPHISM
+            if score > best_score:
+                best_score = score
+                best_tea = tea
+#returning conclusion
+        return best_tea, best_score
